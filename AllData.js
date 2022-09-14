5a3f6093ebd18060df020332782d9398495cd1d1
moumita podder <75267902+moumitapodder@users.noreply.github.com>
2022-09-14 21:11:12 +0530

    Add files via upload

---
+++ AllData.js
@@ -0,0 +1,82 @@
+import office1 from './Image/office1.jpg'
+import office2 from './Image/office2.jpg'
+import office3 from './Image/office3.jpg'
+import Python1 from './Image/python1.jpg'
+import Python2 from './Image/python2.jpg'
+import Python3 from './Image/python3.jpg'
+import React1 from './Image/React1.jpg'
+import React2 from './Image/React2.jpg'
+import fuchka from './Image/fuchka.jpg'
+import fuchka1 from './Image/fuchka1.jpg'
+import momo1 from './Image/momo1.jpg'
+import momo2 from './Image/momo2.jpg'
+
+const Data = [
+    {
+        id:1,
+        image:office1,
+        category:'nonveg'
+    },
+    {
+        id:2,
+        image:office2,
+        category:'nonveg'
+    },
+    {
+        id:3,
+        image:office3,
+        category:'nonveg'
+    },
+    {
+        id:4,
+        image:Python1,
+        category:'desert'
+    },
+    {
+        id:5,
+        image:Python2,
+        category:'desert'
+    },
+    {
+        id:6,
+        image:Python3,
+        category:'desert'
+    },
+    {
+        id:7,
+        image:React1,
+        category:'fish'
+    },
+    {
+        id:8,
+        image:React2,
+        category:'fish'
+    },
+    {
+        id:9,
+        image:fuchka,
+        category: 'fastfood'
+    },
+    {
+
+    
+    id:10,
+    image:fuchka1,
+    category: 'fastfood'
+    },
+    {
+        id:11,
+        image:momo1,
+        category:'fastfood'
+    },
+    {
+
+  
+    id:12,
+    image:momo2,
+    category: 'fastfood'
+    },
+
+];
+
+export default Data;
